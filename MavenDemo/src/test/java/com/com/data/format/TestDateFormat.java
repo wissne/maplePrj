5ae0edbea1efeb8ff8f620645cac67db3590ae00
wissne <m_aple@qq.com>
2015-03-20 21:30:45 +0800

    test

--- MavenDemo/src/test/java/com/com/data/format/TestDateFormat.java
+++ MavenDemo/src/test/java/com/com/data/format/TestDateFormat.java
@@ -15,7 +15,7 @@ public class TestDateFormat {
         String format = new String("yyyy-MM-dd'T'HH:mm:ss.S XXX");
 
         Date date = new Date();
-
+        //
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
         String str = simpleDateFormat.format(date);
         System.out.println(str);
